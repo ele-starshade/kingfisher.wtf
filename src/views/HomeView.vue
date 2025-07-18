cceca7a6c3ee97876eda09347a497f2c3c420360
ele-starshade <ele.starshade@gmail.com>
2025-07-18 20:23:16 +0100

    heh

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data () {
+    return {
+      birbs: []
+    }
+  },
+  async beforeMount () {
+  }
+}
+</script>
+
+<template>
+  <img src="https://images.unsplash.com/photo-1662059765678-4236d2586940" style="width: 100vw; height: 100vh; object-fit: cover;" />
+</template>
